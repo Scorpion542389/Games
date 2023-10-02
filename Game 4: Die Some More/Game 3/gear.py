cadd676d9aa27a8b16b21ccb6e1d8969e1f0eb3d
Scorpion54 <128768365+Scorpion542389@users.noreply.github.com>
2023-10-02 12:51:15 -0500

    Create gear.py

---
+++ Game 4: Die Some More/Game 3/gear.py
@@ -0,0 +1,28 @@
+FarmKnife = 2.2
+empty = 0
+FarmSack = 0
+Short_sword = 2.5
+Sword = 2.75
+Knights_Sword = 3
+Mithril = 5
+Omega = 100
+Iron_plated = 3
+Steel_plated = 3.5
+Copper = 2.85
+Light = 1
+Heavy = 1.5
+Gold_lined = 2.95
+paperUses = 0
+
+LHand = empty
+RHand = empty
+Head = empty
+Legs = FarmSack
+Torso = FarmSack
+Feet = FarmSack
+
+treasure_armor = [Mithril,Iron_plated,Steel_plated,Copper,Light,Heavy,Gold_lined]
+tanames = ["Mithril","Iron Plated","Steel Plated", "Copper","Light","Heavy","Gold Lined"]
+
+treasure_weapon = [FarmKnife,Short_sword,Sword,Knights_Sword,Mithril,Copper,Gold_lined]
+twnames = ["Farm Knife","Short Sword","Broad Sword","Knight's Sword","Mithril","Copper","Gold Lined"]
